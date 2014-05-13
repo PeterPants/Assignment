a3249c411490f85cc6e809633d12bf6c04bcb304
Peter Stone <peterstone@live.co.za>
2014-05-13 14:15:52 +0200

    First Commit

---
+++ functions.h
@@ -0,0 +1,5 @@
+void heapsort(int array[], int size);
+void buildheap(int array[], int size);
+void percolateDown(int heap[], int size, int id);
+void sort(int ar[], int size);
+void printArray(int array[], int size);
